0cdfc6c4009d1b32bb35e668b11d3de4fe1e3b6f
PinD00ol <zabrodin.vlad@hotmail.com>
2024-05-14 11:38:25 +0300

    refactor: rewrite Event as class

--- src/event.hpp
+++ src/event.hpp
@@ -3,12 +3,15 @@
 #include <string>
 
 #include "errors.hpp"
+#include "computer.hpp"
 
-struct Event {
+class Event {
+public:
+    Event(int id, unsigned int table, std::string name, std::string time);
+
+private:
     int idEvent;
     unsigned int numberTable;
     std::string clientName;
     std::string timeEvent;
-
-    Event(int id, unsigned int table, std::string name, std::string time);
 };
