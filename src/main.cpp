cd65132db474593d7cef7439d642734041bb187b
PinD00ol <zabrodin.vlad@hotmail.com>
2024-05-14 11:38:44 +0300

    feat: added queue for events

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <fstream>
+#include <queue>
 
 #include "computer.hpp"
 #include "event.hpp"
 
 int main(int argc, char** argv) {
     std::ifstream file;
-    int tableCount;
-    int lineNumber;
+    unsigned int tableCount, lineNumber, payment;
+    std::string timeStart, timeEnd;
+    std::queue<Event> events;
+
     if(argc > 1)
         file.open(argv[1]);
     else {
@@ -15,9 +18,16 @@ int main(int argc, char** argv) {
         std::exit(1);
     }
 
-
-
+    file >> tableCount >> timeStart >> timeEnd >> payment;
     while (file.good()) {
+        std::string timeEvent, clientName;
+        int idEvent;
+        unsigned int numberTable;
+        file >> timeEvent >> idEvent >> clientName;
+        if(idEvent == 4) file >> numberTable;
+        else numberTable = 0;
+        events.emplace(idEvent, numberTable, clientName, timeEvent);
     }
+    file.close();
     return 0;
 }
