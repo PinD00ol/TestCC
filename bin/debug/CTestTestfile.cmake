02c76c78822eb7338abc03ce55b09107df81ec3f
PinD00ol <75380944+PinD00ol@users.noreply.github.com>
2024-05-22 20:48:27 +0300

    Delete bin directory

--- bin/debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/PinDol/Documents/Laboratories/TestCC
-# Build directory: C:/Users/PinDol/Documents/Laboratories/TestCC/bin/debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/Users/PinDol/Documents/Laboratories/TestCC/bin/debug/tests[1]_include.cmake")
-subdirs("googletest")
--- bin/release/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/PinDol/Documents/Laboratories/TestCC/lib/googletest
-# Build directory: C:/Users/PinDol/Documents/Laboratories/TestCC/bin/release/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
